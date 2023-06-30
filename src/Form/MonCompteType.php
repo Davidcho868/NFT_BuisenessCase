0feb9558a3bfae4e580590f174fd1ab06aac4c19
David Chosson <davidcho868@gmail.com>
2023-06-30 10:14:17 +0200

    06 crea de la page modification de mon compte

---
+++ src/Form/MonCompteType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Adresse;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MonCompteType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            ->add('pseudo')
+            ->add('nom')
+            ->add('prenom')
+            ->add('date_naissance', DateType::class, [
+                'years' => range(1930, date('Y')),
+            ])
+            ->add('is_proprietaire', CheckboxType::class)
+            ->add('adresses', EntityType::class, [
+                'class' => Adresse::class
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
